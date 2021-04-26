9b754d653dc131ebdc70d3dc14313f8e81ccee0a
velesov7493 <velesov7493@yandex.ru>
2021-04-26 18:17:59 +0000

    3. Арифметические операции

---
+++ src/ru/job4j/Calculator.java
@@ -0,0 +1,20 @@
+package ru.job4j;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        int one = 1;
+        int two = 2;
+        int six = 6;
+        int four = 4;
+        int five = 5;
+        int onePlusTwo = one + two;
+        int sixDivTwo = six / two;
+        int fiveMinusTwo = five - two;
+        int fourTimeTwo = four * two;
+        System.out.println("onePlusTwo = "+onePlusTwo);
+        System.out.println("sixDivTwo = "+sixDivTwo);
+        System.out.println("fiveMinusTwo = "+fiveMinusTwo);
+        System.out.println("fourTimeTwo = "+fourTimeTwo);
+    }
+}

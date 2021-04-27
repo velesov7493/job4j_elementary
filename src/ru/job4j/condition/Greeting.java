f0e0256cd14373b757796a012ed93e2cb63dab9c
velesov7493 <velesov7493@yandex.ru>
2021-04-27 13:51:33 +0000

    9.1. String

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class Greeting {
+
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        System.out.println(idea);
+        idea += " But i am a newbie.";
+        int year = 2021;
+        idea += " "+year;
+        System.out.println(idea);
+    }
+}

29cba89d5f42f9a509267f9cd1da672731dffc4e
velesov7493 <velesov7493@yandex.ru>
2021-04-28 11:14:16 +0000

    1. Модульные тесты.

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -2,23 +2,15 @@ package ru.job4j.converter;
 
 public class Converter {
 
-    private static int rubleToEuro(int value) {
+    public static int rubleToEuro(int value) {
         return value / 70;
     }
 
-    private static int rubleToDollar(int value) {
+    public static int rubleToDollar(int value) {
         return value / 60;
     }
 
     public static void main(String[] args) {
-        int in = 140;
-        int expected = 2;
-        int real = rubleToEuro(in);
-        boolean passed = expected == real;
-        System.out.println("140 рублей - это 2 евро. Тест пройден: " + passed);
-        in = 360; expected = 6;
-        real = rubleToDollar(in);
-        passed = expected == real;
-        System.out.println("360 рублей - это 6 долларов. Тест пройден: " + passed);
+        System.out.println("Есть ConverterTest");
     }
 }
---
+++ test/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.converter;
+
+import org.junit.Assert;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ConverterTest {
+
+    @Test
+    public void whenConvert140RubleThen2Euro() {
+        int in = 140;
+        int expected = 2;
+        int real = Converter.rubleToEuro(in);
+        Assert.assertEquals(expected, real);
+    }
+
+    @Test
+    public void whenConvert360RubleTo6Dollars() {
+        int in = 360; int expected = 6;
+        int real = Converter.rubleToDollar(in);
+        Assert.assertEquals(expected, real);
+    }
+}

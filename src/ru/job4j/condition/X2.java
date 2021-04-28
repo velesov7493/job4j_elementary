2d3ccc833fe6b7e3b9b2c5292089d45244c89657
velesov7493 <velesov7493@yandex.ru>
2021-04-28 12:07:03 +0000

    1.2. Входные данные в тесте

---
+++ src/ru/job4j/condition/X2.java
@@ -0,0 +1,12 @@
+package ru.job4j.condition;
+
+public class X2 {
+
+    public static int calc(int a, int b, int c, int x) {
+        return a * x * x + b * x + c;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Значение квадратного уравнения");
+    }
+}

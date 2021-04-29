cc472b0db88c8c36059f3373fb82cc112c56c1c1
velesov7493 <velesov7493@yandex.ru>
2021-04-29 16:33:47 +0000

    4.3.6. Math.abs [#209]

--- src/ru/job4j/condition/ChessBoard.java
+++ src/ru/job4j/condition/ChessBoard.java
@@ -6,10 +6,14 @@ public class ChessBoard {
     // из точки x1, y1 в точку x2, y2
 
     public static int way(int x1, int y1, int x2, int y2) {
-        boolean canWalk = (x1 >= 0) && (x2 >= 0) && (y1 >= 0) && (y2 >= 0);
-        int dx = Math.abs(x2 - x1);
-        int dy = Math.abs(y2 - y1);
-        canWalk &= (dx == dy) && (dx < 8) && (dx > 0);
-        return canWalk ? dx : 0;
+        int result = 0;
+        if (x1 >= 0 && x2 >= 0 && y1 >= 0 && y2 >= 0) {
+            int dx = Math.abs(x2 - x1);
+            int dy = Math.abs(y2 - y1);
+            if (dx == dy && dx < 8) {
+                result = dx;
+            }
+        }
+        return result;
     }
 }

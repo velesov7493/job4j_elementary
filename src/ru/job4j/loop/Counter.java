9647649f1cf48e06a14901d184c95fac6c0b2efb
velesov7493 <velesov7493@yandex.ru>
2021-04-30 15:29:49 +0000

    5.1. Подсчет суммы чётных чисел в диапазоне [#260]

--- src/ru/job4j/loop/Counter.java
+++ src/ru/job4j/loop/Counter.java
@@ -11,19 +11,11 @@ public class Counter {
     }
 
     public static int sumByEven(int start, int finish) {
-
-        // Убираем if из тела цикла,
-        // устанавливаем инкремент в 2,
-        // чтобы не перебирать все числа,
-        // и этим делаем цикл быстрее
-        // минимум в 2 раза
-
         int sum = 0;
-        // Новые границы должны быть четными
-        int newStart = start + start % 2;
-        int newFinish = finish - finish % 2;
-        for (int i = newStart; i <= newFinish; i += 2) {
-            sum += i;
+        for (int i = start; i <= finish; i++) {
+            if (i % 2 == 0) {
+                sum += i;
+            }
         }
         return sum;
     }

bb39542ee535cb4fa063921fae9ad162ba3c83a3
velesov7493 <velesov7493@yandex.ru>
2021-05-01 04:12:49 +0000

    5.7. Ипотека [#156315 #294]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -6,14 +6,13 @@ public class Mortgage {
 
     public static int year(int amount, int salary, double percent) {
         int year = 0;
-        int credit = amount;
-        double dc = -1;
-        while (credit > 0 && dc < 0) {
-            dc = percent * credit / 100 - salary;
-            credit += dc;
+        double delta;
+        do {
+            delta = percent * amount / 100 - salary;
+            amount += delta;
             year++;
-        }
-        if (dc >= 0) {
+        } while (amount > 0 && delta < 0);
+        if (delta >= 0) {
             year = -1;
         }
         return year;

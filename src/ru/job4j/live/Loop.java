785fb600c5e85fc19acdcde2cc94b6197c0cf945
velesov7493 <velesov7493@yandex.ru>
2021-05-01 03:31:11 +0000

    5.9.1. Генерация конструкций в IDEA [#279134 #297]

--- src/ru/job4j/live/Loop.java
+++ src/ru/job4j/live/Loop.java
@@ -3,7 +3,7 @@ package ru.job4j.live;
 public class Loop {
 
     public static void main(String[] args) {
-        for (char i = 0x20; i < 0x80; i++) {
+        for (int i = 0; i < 20; i++) {
             System.out.println(i);
         }
     }

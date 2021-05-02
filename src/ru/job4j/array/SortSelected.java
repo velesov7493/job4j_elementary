84598a9ef44b45d98a08e421976786982d618fd2
velesov7493 <velesov7493@yandex.ru>
2021-05-02 08:37:43 +0000

    6.7.1. Моно строка в матрице. [#214126] Незначительные изменения в сортировке выборкой.

--- src/ru/job4j/array/SortSelected.java
+++ src/ru/job4j/array/SortSelected.java
@@ -4,8 +4,8 @@ public class SortSelected {
 
     public static int[] sort(int[] data) {
         for (int i = 0; i < data.length; i++) {
-            int min = MinDiapason.findMin(data, i, data.length);
-            int j = FindLoop.indexOf(data, min, i, data.length);
+            int min = MinDiapason.findMin(data, i, data.length - 1);
+            int j = FindLoop.indexOf(data, min, i, data.length - 1);
             if (j != i) {
                 SwitchArray.swap(data, i, j);
             }

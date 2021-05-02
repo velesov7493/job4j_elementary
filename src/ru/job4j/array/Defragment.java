3c7b2a5b250abf3b643e7b75d138fb1458e9bf03
velesov7493 <velesov7493@yandex.ru>
2021-05-02 16:50:01 +0000

    6.8. Дефрагментация массива [#225]

---
+++ src/ru/job4j/array/Defragment.java
@@ -0,0 +1,38 @@
+package ru.job4j.array;
+
+public class Defragment {
+
+    private static void exchange(String[] data, int source, int dest) {
+        String tmp = data[source];
+        data[source] = data[dest];
+        data[dest] = tmp;
+    }
+
+    private static int indexOfNotNullAfter(String[] array, int start) {
+        int result = -1;
+        if (start >= array.length || start < 0) {
+            return result;
+        }
+        for (int i = start; i < array.length; i++) {
+            if (array[i] != null) {
+                result = i;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static String[] compress(String[] array) {
+        for (int index = 0; index < array.length; index++) {
+            if (array[index] == null) {
+                int notNullPoint = indexOfNotNullAfter(array, index + 1);
+                if (notNullPoint < 0) {
+                    break;
+                } else {
+                    exchange(array, index, notNullPoint);
+                }
+            }
+        }
+        return array;
+    }
+}
